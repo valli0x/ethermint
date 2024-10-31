b9603a6c18a5c16591fbc4ebe6c16eba5682b7c2
valli0x <youarethesame1821@gmail.com>
2024-10-31 14:42:13 +0300

    remove l.Close from stopping json-rpc server

--- server/json_rpc.go
+++ server/json_rpc.go
@@ -149,7 +149,7 @@ func StartJSONRPC(ctx context.Context, srvCtx *server.Context,
 			if err := httpSrv.Shutdown(context.Background()); err != nil {
 				logger.Error("failed to shutdown JSON-RPC server", "error", err.Error())
 			}
-			return ln.Close()
+			return err
 
 		case err := <-errCh:
 			if err == http.ErrServerClosed {
